57780c2b6d8c57ac1739cc4bf44cf4619f0c2c4d
Nasimul-Rahman <nasimul@cynergon.net>
2023-09-10 11:49:30 +0600

    Style Overhaul

--- deployment_practice/settings.py
+++ deployment_practice/settings.py
@@ -142,6 +142,10 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
